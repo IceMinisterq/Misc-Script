3bba7271387706b1b87ab2250b635256c5f84166
IceMinisterq <86623018+IceMinisterq@users.noreply.github.com>
2023-08-31 21:26:08 +0000

    Create adonis ac bypass.lua

---
+++ adonis ac bypass.lua
@@ -0,0 +1,28 @@
+
+local function isAdonisAC(table) -- basic stupid checks
+	return rawget(table, "Detected") and typeof(rawget(table, "Detected")) == "function" and rawget(table, "RLocked")
+end
+
+warn("------------------------------")
+
+for _, v in next, getgc(true) do
+	if typeof(v) == "table" and isAdonisAC(v) then
+		warn(warn, "founded")
+		for i, v in next, v do
+			warn(print, i, typeof(v))
+			if rawequal(i, "Detected") then
+				warn(warn, "^^^^^^^")
+				local old;
+				old = hookfunction(v, function(action, info, nocrash)
+					if rawequal(action, "_") and rawequal(info, "_") and rawequal(nocrash, true) then
+						-- warn("checkup")
+						return old(action, info, nocrash)
+					end
+					warn(warn, "detected for :", action, info, nocrash)
+					return task.wait(9e9)
+				end)
+				warn("bypassed adonis ac")
+			end
+		end
+	end
+end
